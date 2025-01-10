cdfbc6fdf016aca14ba64a5e6509e71c381d2e5b
Dori Minante <dorim.dev@gmail.com>
2025-01-10 18:48:40 -0300

    calculos

--- src/ImpostoDeRenda.java
+++ src/ImpostoDeRenda.java
@@ -19,7 +19,7 @@ public class ImpostoDeRenda {
         double salarioMensal = rendaSalario / 12;
         double impostoSalario = 0.0;
 
-        if(salarioMensal > = 3000 && salarioMensal < 5000) {
+        if(salarioMensal >= 3000 && salarioMensal < 5000) {
             impostoSalario = rendaSalario * 0.10;
         } else if (salarioMensal >= 5000) {
             impostoSalario = rendaSalario * 0.20;
@@ -36,10 +36,32 @@ public class ImpostoDeRenda {
 
         // Deduções
 
-        
+        double maximoDedutivel = impostoBrutoTotal * 0.30;
+        double gastosDedutiveis = gastosMedicos + gastosEducacionais;
 
+        if (gastosDedutiveis > maximoDedutivel) {
+            gastosDedutiveis = maximoDedutivel;
+        }
 
+        // Imposto devido
+        double impostoDevido = impostoBrutoTotal - gastosDedutiveis;
 
+        // Resultados
+        System.out.println("**************************************************");
+        System.out.println("\nRELATÓRIO DE IMPOSTO DE RENDA\n");
+        System.out.println("**************************************************\n");
+        System.out.println("CONSOLIDADO DE RENDA: ");
+        System.out.println("Imposto sobre salário: R$ " + impostoSalario);
+        System.out.println("Imposto sobre serviços: R$ " + impostoServico);
+        System.out.println("Imposto sobre ganho de capital: R$ \n" + impostoGanhoCapital);
+        System.out.println("DEDUÇÕES: ");
+        System.out.println("Máximo dedutível: R$ " + maximoDedutivel);
+        System.out.println("Gastos dedutíveis: R$ \n" + gastosDedutiveis);
+        System.out.println("RESUMO: ");
+        System.out.println("Imposto bruto total: R$ " + impostoBrutoTotal);
+        System.out.println("Abatimento: " + gastosDedutiveis);
+        System.out.println("Imposto devido: R$ " + impostoDevido);
 
+        scanner.close();
     }
 }
